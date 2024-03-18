5da29e3af0234bdf0bdd9f053e6e5db6b84a13ae
AndreIglesias <ciglesia@student.42.fr>
2024-03-18 23:04:57 +0100

    token ignore + kubectl install

--- p1/setup/k3s_agent.sh
+++ p1/setup/k3s_agent.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+while [ ! -f /vagrant/token ]; do sleep 1; done
+
 TOKEN=$(cat /vagrant/token)
 IPMaster="192.168.56.110"
 curl -sfL https://get.k3s.io | K3S_URL="https://${IPMaster}:6443" K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
--- p1/setup/k3s_master.sh
+++ p1/setup/k3s_master.sh
@@ -4,3 +4,8 @@ echo 'export KUBECONFIG=/etc/rancher/k3s/k3s.yaml' >> /home/vagrant/.profile
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 k3S_CLUSTER_INIT=1 INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
 
 sudo cp /var/lib/rancher/k3s/server/token /vagrant/
+sudo chown vagrant:vagrant /vagrant/token
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
