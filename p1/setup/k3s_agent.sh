42a910b6a7d4f3c1baf0c3d5c2d4da3e425c7a2d
AndreIglesias <ciglesia@student.42.fr>
2024-03-18 22:21:18 +0100

    agent script

--- p1/setup/k3s_agent.sh
+++ p1/setup/k3s_agent.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-while [ ! -f /vagrant/token ]; do
-    echo "Waiting for token..."
-    sleep 5
-done
-
 TOKEN=$(cat /vagrant/token)
 IPMaster="192.168.56.110"
 curl -sfL https://get.k3s.io | K3S_URL="https://${IPMaster}:6443" K3S_TOKEN=$TOKEN INSTALL_K3S_EXEC='--flannel-iface=eth1' sh -
